54c2fa2653aa111be42c65d0ce4ec3efe3e7b473
jonathansa-98 <daw2jsuarezalvarez@iesjoanramis.org>
2019-02-02 16:44:38 +0100

    mini optimizacion

--- src/ProblemaBElementos.java
+++ src/ProblemaBElementos.java
@@ -4,13 +4,16 @@
  */
 public class ProblemaBElementos {
 	static java.util.Scanner in;
+        // excluidas vocales
         static String letras = "bBcCdDfFgGhHjJkKlLmMnNpPqQrRsStTvVwWxXyYzZ";
                 
 	public static void casoDePrueba(){
-            String line;
-            line = in.nextLine();
+            
+            String line = in.nextLine();
             int[] pos = new int[line.length()];
             String consonante;
+            boolean orden = true;
+            
             for (int i = 0; i < line.length(); i++) {
                 consonante = line.substring(i, i+1);
                 if(letras.contains(consonante)){
@@ -18,25 +21,20 @@ public class ProblemaBElementos {
                 } else{
                     pos[i] = -1;
                 }
-                if(pos[i]!=-1){
-                    System.out.println(pos[i]);
-                }
-                // pos[i] <= pos[i+1] ...
             }
-            boolean orden = true;
             for (int e = 0; e < pos.length-1 & orden; e++) {
                 if(pos[e]!=-1){
+                    // si: pos[e] <= pos[e+1] ... "ERROR". Ej: BC "ERROR"
                     if(!(pos[e] <= pos[e+1])) orden = false;  
                 }
             }
-            if(orden) System.out.println("OK");
+            if (orden) System.out.println("OK");
             else System.out.println("ERROR");
 	}
 	
 	public static void main(String args[]){
             in = new java.util.Scanner(System.in);
-            int numCasos;
-            numCasos=in.nextInt();
+            int numCasos = in.nextInt();
             for (int i = 0; i <	numCasos; i++){
                 casoDePrueba();
             }
