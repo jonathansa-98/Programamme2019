9f4b2aab8a4a2aa8fb0de300c9439552d2dfd8eb
jonathansa-98 <daw2jsuarezalvarez@iesjoanramis.org>
2019-02-02 16:30:35 +0100

    ProblemaB funciona, falta optimizar

--- src/ProblemaBElementos.java
+++ src/ProblemaBElementos.java
@@ -3,5 +3,42 @@
  * @author Jonathan
  */
 public class ProblemaBElementos {
-    
+	static java.util.Scanner in;
+        static String letras = "bBcCdDfFgGhHjJkKlLmMnNpPqQrRsStTvVwWxXyYzZ";
+                
+	public static void casoDePrueba(){
+            String line;
+            line = in.nextLine();
+            int[] pos = new int[line.length()];
+            String consonante;
+            for (int i = 0; i < line.length(); i++) {
+                consonante = line.substring(i, i+1);
+                if(letras.contains(consonante)){
+                    pos[i] = letras.indexOf(consonante);
+                } else{
+                    pos[i] = -1;
+                }
+                if(pos[i]!=-1){
+                    System.out.println(pos[i]);
+                }
+                // pos[i] <= pos[i+1] ...
+            }
+            boolean orden = true;
+            for (int e = 0; e < pos.length-1 & orden; e++) {
+                if(pos[e]!=-1){
+                    if(!(pos[e] <= pos[e+1])) orden = false;  
+                }
+            }
+            if(orden) System.out.println("OK");
+            else System.out.println("ERROR");
+	}
+	
+	public static void main(String args[]){
+            in = new java.util.Scanner(System.in);
+            int numCasos;
+            numCasos=in.nextInt();
+            for (int i = 0; i <	numCasos; i++){
+                casoDePrueba();
+            }
+	}
 }
--- src/ProblemaCPrimos.java
+++ src/ProblemaCPrimos.java
@@ -13,7 +13,7 @@ public class ProblemaCPrimos {
         return true;
     }
     
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         byte casos;
         casos = sc.nextByte();
@@ -36,5 +36,5 @@ public class ProblemaCPrimos {
             }
             System.out.println(cont);
         }
-    }
+    }*/
 }
