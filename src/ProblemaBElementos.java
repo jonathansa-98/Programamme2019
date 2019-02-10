33854c8258a986d6eb48a604d4a2fc3ab7ec1cfa
jonathansa-98 <daw2jsuarezalvarez@iesjoanramis.org>
2019-02-10 17:42:19 +0100

    Problema B done optimizado

--- src/ProblemaBElementos.java
+++ src/ProblemaBElementos.java
@@ -4,28 +4,23 @@
 public class ProblemaBElementos {
     static java.util.Scanner in;
     static String letras = "AEIOU";
-    static StringBuilder sb = new StringBuilder();
 
     public static void casoDePrueba(){
-        String line = in.next();
-        line = line.toUpperCase();
+        String line = in.next().toUpperCase();
         String consonante;
-        String aux;
+        char letra_anterior = 0;
         boolean orden = false;
-        for (int i = 0; i < line.length(); i++) {
+        for (int i = 0; i < line.length() && !orden; i++) {
             consonante = line.substring(i, i+1);
+            // filtra consonantes
             if(!letras.contains(consonante)){
-               sb.append(consonante);
+               // comprueva la consonante anterior con la siguiente.
+               if(!(letra_anterior <= line.charAt(i))) orden = true;
+               letra_anterior = consonante.charAt(0);
             }
         }
-        for (int e = 0; e < sb.length()-1 & !orden; e++) {
-            // si: sb.charAt(e) <= sb.charAt(e+1) ... 
-            // "ERROR".     Ej: BC "ERROR"
-            if(!(sb.charAt(e) <= sb.charAt(e+1))) orden = true;
-        }
-        if (orden && sb.length() >= 2) System.out.println("OK");
+        if (orden && line.length() >= 2) System.out.println("OK");
         else System.out.println("ERROR");
-        sb.delete(0, sb.length());
     }
 
     public static void main(String args[]){
